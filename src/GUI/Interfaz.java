915e31d87646f9f6604a25c722d94129f87a0042
Eze <ezequielzucoli@hotmail.com>
2018-09-02 19:40:40 -0300

    Principio de la interfaz

--- src/GUI/Interfaz.java
+++ src/GUI/Interfaz.java
@@ -1,5 +1,54 @@
 package GUI;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.Panel;
+import javax.swing.JSeparator;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.BorderLayout;
+
 public class Interfaz {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaz window = new Interfaz();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interfaz() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+	}
 
 }
